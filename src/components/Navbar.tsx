575f70f71db20c62710c54305a8a729dd2045990
Nusab Taha <nusabtaha33@gmail.com>
2024-10-18 04:09:36 +0600

    logo is a link

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,7 +6,7 @@ const Navbar = () => {
   return (
     <>
       <nav className="mx-2 flex max-w-screen-xl items-center justify-between py-2 xl:mx-auto">
-        <div className="flex items-center justify-center gap-1 md:gap-1.5">
+        <Link href="/" className="flex items-center justify-center gap-1 md:gap-1.5">
           <svg
             className="h-8 w-8 text-[#f1502f] md:h-10 md:w-10"
             fill="currentColor"
@@ -16,7 +16,7 @@ const Navbar = () => {
             <path d="M439.55 236.05L244 40.45a28.87 28.87 0 0 0-40.81 0l-40.66 40.63 51.52 51.52c27.06-9.14 52.68 16.77 43.39 43.68l49.66 49.66c34.23-11.8 61.18 31 35.47 56.69-26.49 26.49-70.21-2.87-56-37.34L240.22 199v121.85c25.3 12.54 22.26 41.85 9.08 55a34.34 34.34 0 0 1-48.55 0c-17.57-17.6-11.07-46.91 11.25-56v-123c-20.8-8.51-24.6-30.74-18.64-45L142.57 101 8.45 235.14a28.86 28.86 0 0 0 0 40.81l195.61 195.6a28.86 28.86 0 0 0 40.8 0l194.69-194.69a28.86 28.86 0 0 0 0-40.81z"></path>
           </svg>
           <p className="-mt-1 font-black md:text-2xl text-xl">git ignore</p>
-        </div>
+        </Link>
         <div className="flex items-center justify-center gap-2">
           <Link href="">
             <svg
